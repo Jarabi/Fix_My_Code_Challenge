9d08f7d8342e64bc84bc965ca429a3518b085981
Alex Jarabi <jarabialex@gmail.com>
2023-09-19 14:22:10 +0000

    Fixed perimeter of square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+''' Class definition for a square '''
+
 
 class square():
-    
+    '''
+    Definition of a square
+    '''
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Initialization for arguments """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +20,13 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Perimeter of square """
         return (self.width * 4)
 
     def __str__(self):
+        """ return string representation """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
